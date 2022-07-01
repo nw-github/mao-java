33817ddcf175306dceddf7283d54a1c473b3ee54
Nathaniel Williams <po.box.nate@gmail.com>
2022-07-01 15:48:06 -0400

    remove phrases

--- src/main/java/com/dog/game/Card.java
+++ src/main/java/com/dog/game/Card.java
@@ -1,23 +1,9 @@
 package com.dog.game;
 
-enum Suit {
-    HEARTS, DIAMONDS, SPADES, CLUBS,
-};
+enum Suit { HEARTS, DIAMONDS, SPADES, CLUBS };
+enum Face { A, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING };
 
-enum Face {
-    A, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
-};
-
-public class Card {
-    private final Suit mSuit;
-    private final Face mFace;
-
-    public Card(Suit suit, Face face) {
-        mSuit = suit;
-        mFace = face;
-    }
-
-    public Suit getSuit() { return mSuit; }
-    
-    public Face getFace() { return mFace; }
-}
+public record Card(
+    Suit suit,
+    Face face
+) { }
--- src/main/java/com/dog/game/Game.java
+++ src/main/java/com/dog/game/Game.java
@@ -2,18 +2,19 @@ package com.dog.game;
 
 import java.util.Arrays;
 
+import com.dog.game.net.Player;
+
 public class Game {
     private static final int STARTING_CARDS = 7;
-    private static final long SPEECH_TIME = 3; // seconds
 
     private Deck     mDeck      = new Deck();
     private Deck     mDiscard   = new Deck();
     private int      mTurn      = 0;
     private int      mPlayOrder = +1; // +1 for clockwise, -1 for counterclockwise
     private Player[] mPlayers;
-    
-    public Game(int players) {
-        mPlayers = new Player[players]; // TODO: players will probably be passed in as an argument when networking is done
+
+    public Game(Player[] players) {
+        mPlayers = players;
 
         for (var suit : Suit.values())
             for (var face : Face.values())
@@ -24,18 +25,18 @@ public class Game {
 
         for (var player : mPlayers)
             for (int i = 0; i < STARTING_CARDS; i++)
-                player.cards.take(mDeck);
+                player.getCards().take(mDeck);
+    }
+
+    public void punish(Player player, String reason) {
+        player.getCards().take(mDeck);
+
+        // TODO: broadcast the reason in chat? 
     }
 
     private boolean isValidMove(Card card) {
-        return (card.getFace() == mDiscard.top().getFace()) ||
-            (card.getSuit() == mDiscard.top().getSuit());
-    }
-
-    private void punish(Player player, String reason) {
-        player.cards.take(mDeck);
-
-        // TODO: broadcast the reason in chat? 
+        return (card.face() == mDiscard.top().face()) ||
+            (card.suit() == mDiscard.top().suit());
     }
 
     private void nextTurn() {
@@ -46,92 +47,24 @@ public class Game {
             mTurn = 0;
     }
 
-    private void checkRequirePhrase(Player player, Card card) {
-        switch (card.getFace()) {
-        case JACK:
-            player.phrase = "here comes the badger";
-            break;
-        case QUEEN:
-            player.phrase = "all hail the queen";
-            break;
-        case KING:
-            player.phrase = "all hail the king";
-            break;
-        default:
-            break;
-        }
-
-        if (card.getSuit() == Suit.SPADES) {
-            switch (card.getFace()) {
-            case JACK:
-            case QUEEN:
-            case KING:
-                player.phrase += " of spades";
-                break;
-            default:
-                player.phrase = String.format("%s of spades", card.getSuit().toString());
-                break;
-            }
-        }
-
-        if (player.cards.size() == 1)
-            player.phrase = "mao";
-
-        if (player.cards.size() == 0)
-            player.phrase = "all hail the king of mao";
-        
-        // --------------------------
-        
-        if (player.phrase != null) {
-            if (player.checkThread != null) {
-                try {
-                    var thr = player.checkThread;
-                    thr.interrupt();
-                    thr.join();
-                } catch (InterruptedException ex) {}
-            }
-
-            player.checkThread = new Thread(() -> {
-                try {
-                    Thread.sleep(SPEECH_TIME * 1000);
-                } catch (InterruptedException ex) {
-                    // TODO: there should be some check to ensure the interruption
-                    //       didn't come from something outside our control, and return
-                    //       to sleeping if it did
-                }
-
-                if (player.phrase != null) {
-                    punish(player, String.format("Failure to say '%s'.", player.phrase));
-                    player.phrase = null;
-                }
-
-                player.checkThread = null;
-            });
-
-            player.checkThread.start();
-        }
-    }
-
     public void play(Player player, int cardIndex) throws IllegalArgumentException {
         int index = Arrays.asList(mPlayers).indexOf(player);
-        if (index == -1 || cardIndex >= player.cards.size())
+        if (index == -1 || cardIndex >= player.getCards().size())
             throw new IllegalArgumentException("Player or card index is invalid.");
 
-        var card = player.cards.get(cardIndex);
+        var card  = player.getCards().get(cardIndex);
         var valid = isValidMove(card);
 
-        mDiscard.take(player.cards, cardIndex);
+        mDiscard.take(player.getCards(), cardIndex);
         if (index != mTurn) {
             punish(player, "Playing out of turn.");
             return;
         }
 
         if (!valid) {
-            punish(player, "...");  // TODO: i dont remember what the reason given for an invalid move usually is
+            punish(player, "Wrong card.");  // TODO: i dont remember what the reason given for an invalid move usually is
             return;
         }
-
-        checkRequirePhrase(player, card);
 
         // TODO: other rules i cant remember the specifics of right now:
         //       there's a face that reverses the order of play
